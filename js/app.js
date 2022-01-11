6d2d80780062790888dbee752af768c7b5c05607
Daniel Ochoa <daniel.romero48@uabc.edu.mx>
2022-01-11 12:02:36 -0800

    Validacion del formulario

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ eventListeners();
 
 function eventListeners() {
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+    formulario.addEventListener('submit', agregarGasto);
 }
 // Classes
 class Presupuesto {
@@ -28,12 +29,34 @@ class UI {
         document.querySelector('#restante').textContent = restante;
 
     }
+    imprimirAlerta(mensaje, tipo) {
+        // Crear el div
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert');
+
+        if(tipo === 'error') {
+            divMensaje.classList.add('alert-danger');
+        } else {
+            divMensaje.classList.add('alert-success');
+        }
+
+        // Mensaje de error
+        divMensaje.textContent = mensaje;
+
+        // Insertar en el HTML
+        document.querySelector('.primario').insertBefore( divMensaje, formulario);
+
+        // Quitar del HTML
+
+        setTimeout(() => {
+            divMensaje.remove();
+        }, 3000);
+    }
 }
 
 // Instanciar
 
 const ui = new UI();
-
 let presupuesto;
 
 
@@ -52,4 +75,24 @@ function preguntarPresupuesto() {
     console.log(presupuesto);
 
     ui.insertarPresupuesto( presupuesto );
+}
+
+// Añade gastos
+function agregarGasto(e) {
+    e.preventDefault();
+
+    // Leer los datos del formulario
+    const nombre = document.querySelector('#gasto').value;
+    const cantidad = document.querySelector('#cantidad').value;
+
+    // Validar
+    if (nombre === '' || cantidad === '') {
+        ui.imprimirAlerta('Ambos campos son obligatorios', 'error');
+        return;
+    } else if ( cantidad <= 0 || isNaN(cantidad) ) {
+        ui.imprimirAlerta('Cantidad no válida', 'error')
+        return;
+    }
+
+    console.log('Agregando gasto');
 }
