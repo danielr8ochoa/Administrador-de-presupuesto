0d6634944f6450bb71da50aaea861758856eb6ea
Daniel Ochoa <daniel.romero48@uabc.edu.mx>
2022-01-11 10:24:59 -0800

    Creando e instanciando la clase de Presupuesto

--- js/app.js
+++ js/app.js
@@ -5,21 +5,38 @@ const gastoListado = document.querySelector('#gastos ul');
 // Event1000os
 
 eventListeners();
+
 function eventListeners() {
     document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
 }
 // Classes
+class Presupuesto {
+    constructor(presupuesto) {
+        this.presupuesto = Number(presupuesto);
+        this.restante = Number(presupuesto);
+        this.gastos = [];
+    }
+}
+
+class UI {
+
+}
+
+let presupuesto;
+
 
 // Funciones
 function preguntarPresupuesto() {
     const presupuestoUsuario = prompt('¿Cuál es tu presupuesto');
-    console.log(Number(presupuestoUsuario));
 
     if (presupuestoUsuario === '' || presupuestoUsuario === null || isNaN(presupuestoUsuario) || presupuestoUsuario <= 0) {
         window.location.reload();
 
     } else {
-        
+
     }
+
+    presupuesto = new Presupuesto(presupuestoUsuario);
+    console.log(presupuesto);
 
 }
